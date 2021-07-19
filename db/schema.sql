73b1c5e33bad243387c3391117c6c3ab7268e720
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-07-19 18:44:12 -0500

    db schema fixed

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,24 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
+CREATE TABLE roles (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NULL,
   salary DECIMAL (6,0),
-  department_id INTEGER NOT NULL REFERENCES department(id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id)  
-  REFERENCES department(id)  
-  ON DELETE CASCADE  
-  ON UPDATE CASCADE  
+  department_id INT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)   
 );
 
 CREATE TABLE employee (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL REFERENCES role(id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id)  
-  REFERENCES role(id)  
-  ON DELETE CASCADE  
-  ON UPDATE CASCADE  
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),  
+  FOREIGN KEY (manager_id) REFERENCES employee(id),  
 );
 
